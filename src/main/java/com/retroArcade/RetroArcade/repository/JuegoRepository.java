5a668871eff74bfabc5bedae3cc9c3122281a411
Carolina Birolo <carobirolo@gmail.com>
2025-10-28 11:47:00 -0300

    agregue paginacion y que la busqueda funcione

--- src/main/java/com/retroArcade/RetroArcade/repository/JuegoRepository.java
+++ src/main/java/com/retroArcade/RetroArcade/repository/JuegoRepository.java
@@ -1,8 +1,15 @@
 package com.retroArcade.RetroArcade.repository;
 
 import com.retroArcade.RetroArcade.model.Juego;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JuegoRepository extends JpaRepository<Juego, Long> {}
+public interface JuegoRepository extends JpaRepository<Juego, Long> {
+    Page<Juego> findByCategoriaNombreIgnoreCase(String nombre, Pageable pageable);
+    Page<Juego> findByCategoriaId(Long categoriaId, Pageable pageable);
+
+    Page<Juego> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
+}
